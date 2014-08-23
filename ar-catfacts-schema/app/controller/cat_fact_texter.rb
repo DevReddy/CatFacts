68969d1251bcda47768be80dd6ae97b2757bb69e
Ryan Eddy <ryaneddydev@gmail.com>
2014-08-22 17:14:15 -0700

    Can now continuously spam texts. Currently still have friends.

--- ar-catfacts-schema/app/controller/cat_fact_texter.rb
+++ ar-catfacts-schema/app/controller/cat_fact_texter.rb
@@ -2,10 +2,6 @@ require_relative '../../db/config'
 require_relative '../models/dataparser'
 require 'twilio-ruby'
 
-
-rand_fact_id = rand(Catfact.count)
-cat_fact = Catfact.find_by(id: rand_fact_id).catfact
-puts Dir.pwd
 text = TextParser.parse_txt('./app/models/student_numbers.txt')
 numbers = TextParser.convert_to_array_of_arrays(text)
 
@@ -14,6 +10,29 @@ auth_token = '1242572856dbaca3a0faf7418f8cd902'
 
 @client = Twilio::REST::Client.new account_sid, auth_token
 
-@message = @client.account.messages.create({:to => numbers.sample,
-                                   :from => "+14154668666",
-                                   :body => "Hey! Thanks for subscribing to Cat Facts! Here's your Cat Fact! Mee-WOW!\n" + cat_fact})
+#LOOP IT!
+Catfact.count.times do
+
+  rand_fact_id = rand(Catfact.count + 1)
+  while rand_fact_id <= 0 || rand_fact_id > Catfact.count
+    rand_fact_id = rand(Catfact.count + 1)
+  end
+  cat_fact = Catfact.find_by(id: rand_fact_id).catfact
+  # puts Dir.pwd
+
+
+  send_num = numbers.sample
+  # send_num = ["12022994405"] #Sherif
+
+  body = "Hey! Thanks for subscribing to Cat Facts! Here's your Cat Fact! Mee-WOW!\n"
+  # body = "Hey Sherif! "
+
+  puts "Sending to #{send_num}!"
+
+
+  @message = @client.account.messages.create({:to => send_num,
+                                     :from => "+14154668666",
+                                     :body => body + cat_fact})
+
+  sleep(7)
+end
